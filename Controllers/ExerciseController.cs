26f897176eadc16f7b9e5478bc3626bbdeed11e0
Cayden Miller <cayden.miller.695@gmail.com>
2024-10-21 21:20:06 -0500

    Add project files.

---
+++ Controllers/ExerciseController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Fitness.Controllers
+{
+    public class ExerciseController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult AddRoutine()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/ExerciseRoutine.cs
@@ -0,0 +1,24 @@
+﻿namespace Fitness.Models
+{
+    public class Exercise
+    {
+        public int Id { get; set; }
+        public DateTime dateRecorded { get; set; }
+
+        public decimal? distanceRan { get; set; }
+        public decimal time { get; set; }
+
+        public decimal? weightBenched { get; set; }
+        public decimal? weightDeadlift { get; set; }
+        public decimal? weightCurl { get; set; }
+
+        public decimal findMPH()
+        {
+            if (distanceRan.HasValue && time > 0)
+            {
+                return distanceRan.Value / time;
+            }
+            return 0;
+        }
+    }
+}
